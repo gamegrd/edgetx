83e143817196f798400a44e99ce3076251eb56e9
3djc <3djc@gh.com>
2024-05-04 10:48:34 +0200

    feat:default to mute

--- radio/src/opentx.cpp
+++ radio/src/opentx.cpp
@@ -360,7 +360,7 @@ void generalDefault()
   g_eeGeneral.pwrOffSpeed = 2;
 #endif
 
-#if defined(MANUFACTURER_RADIOMASTER)
+#if defined(MANUFACTURER_RADIOMASTER) || defined(MANUFACTURER_JUMPER)
   g_eeGeneral.audioMuteEnable = 1;
 #endif
 
